28e72f400944426b219571b88acc247b58b78450
Danil <uxample@gmail.com>
2021-06-08 14:33:27 +0300

    git kraken test

--- js/massives(18).js
+++ js/massives(18).js
@@ -5,3 +5,21 @@ const arr = [1, 2, 3, 6, 8];
 arr.push(10);
 
 console.log(arr);
+
+
+
+for (let i = 0; i < arr.length, i++;) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value);
+}
+
+let z = 'chunga';
+for (let ztest of z) {
+    console.log(ztest);
+}
+
+arr.forEach(function (item, i, arr) {
+            console.log(`${i}: ${item} внутри массива ${arr}`)
